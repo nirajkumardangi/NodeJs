5cdf324cc4a648ef40b0d47be8ae5d54424bf6d2
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-11-30 19:41:52 +0530

    Exporting patterns in node module

--- 2-Module/app.js
+++ 2-Module/app.js
@@ -1,5 +1,9 @@
 const product = require('./math/product');
-const sum = require('./math/sum');
+const {sum, PI} = require('./math/sum');
+
+// const sum = require('./math/sum');
+// const PI = require('./math/sum');
 
 console.log('🚀 ~ product:', product(4, 5)); // 20
 console.log('🚀 ~ sum:', sum(4, 5));  // 9
+console.log('🚀 ~ PI:', PI)  // 3.14
--- 2-Module/math/sum.js
+++ 2-Module/math/sum.js
@@ -1,5 +1,15 @@
+const PI = 3.14;
+
 function sum(...numbers) {
   return numbers.reduce((curr, acc) => curr + acc);
 }
 
-module.exports = sum;
+// pattern 1
+// module.exports = sum;
+
+// pattern 2
+// module.exports = { sum, PI };
+
+// pattern 3
+module.exports.sum = sum;
+module.exports.PI = PI;
