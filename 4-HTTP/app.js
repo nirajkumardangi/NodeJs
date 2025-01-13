72a5089dc510be9f82451baaf6ebc3dc8c76d4df
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2025-01-13 17:12:17 +0530

    creaing server

---
+++ 4-HTTP/app.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+const server = http.createServer(function (req, res) {
+  res.end('Serer is is Running wdhbhjsbdb jbdbd cbbbbbbbbbbbbbbbbbbbbb');
+});
+
+server.listen(3000);
